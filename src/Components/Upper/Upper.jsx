46f8a34ba03b6192a331e369e7543a38983fa530
Matthew King <matthewkingstudent@gmail.com>
2023-09-04 16:35:21 -0400

    finished curving text, began blinker

--- src/Components/Upper/Upper.jsx
+++ src/Components/Upper/Upper.jsx
@@ -6,21 +6,20 @@ function Upper() {
         <div className="Upper">
             <div className="title">
                 <Blob />
-                <div className="container">
-                    <div className="box">
-                        <p style={{ "--i": -6 }}>M</p>
-                        <p style={{ "--i": -5 }}>A</p>
-                        <p style={{ "--i": -4 }}>T</p>
-                        <p style={{ "--i": -3 }}>T</p>
-                        <p style={{ "--i": -2 }}>H</p>
-                        <p style={{ "--i": -1 }}>E</p>
-                        <p style={{ "--i": 0 }}>W</p>
-                        <p style={{ "--i": 1 }}> </p>
-                        <p style={{ "--i": 2 }}>K</p>
-                        <p style={{ "--i": 3 }}>I</p>
-                        <p style={{ "--i": 4 }}>N</p>
-                        <p style={{ "--i": 5 }}>G</p>
-                    </div>
+                <div className="box">
+                    <p style={{ "--i": -6 }}>M</p>
+                    <p style={{ "--i": -5 }}>A</p>
+                    <p style={{ "--i": -4 }}>T</p>
+                    <p style={{ "--i": -3 }}>T</p>
+                    <p style={{ "--i": -2 }}>H</p>
+                    <p style={{ "--i": -1 }}>E</p>
+                    <p style={{ "--i": 0 }}>W</p>
+                    <p style={{ "--i": 1 }}> </p>
+                    <p style={{ "--i": 2 }}>K</p>
+                    <p style={{ "--i": 3 }}>I</p>
+                    <p style={{ "--i": 4 }}>N</p>
+                    <p style={{ "--i": 5 }}>G</p>
+                    <p style={{ "--i": 6 }}/>
                 </div>
             </div>
         </div>

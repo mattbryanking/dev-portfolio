6b8475ee15d7b07fd754c31f0e6d3b8510b7c0ce
Matthew King <matthewkingstudent@gmail.com>
2023-09-07 23:08:27 -0400

    added icons to contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -21,7 +21,6 @@ function Contact() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(formData);
 
         const { firstName, lastName, email, message } = formData;
 
@@ -36,7 +35,6 @@ function Contact() {
     // the standard invalid popups should probably exist on mobile
     const handleInvalid = (e) => {
         e.preventDefault();
-        console.log(formData);
 
         const email = formData[2];
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -51,9 +49,54 @@ function Contact() {
         <div className="Contact">
             <h3 className="title">Contact</h3>
             <div className="contact-content">
-                <h1>
-                    Let&apos;s Get <br /> In Touch.
-                </h1>
+                <div className="left-contact-container">
+                    <h1>
+                        Let&apos;s Get <br /> In Touch.
+                    </h1>
+                    <div className="icon-container">
+                        <a>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="30"
+                                height="30"
+                                fill="white"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z" />
+                            </svg>
+                        </a>
+                        <a
+                            href="https://github.com/mattbryanking"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="30"
+                                height="30"
+                                fill="white"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                            </svg>
+                        </a>
+                        <a
+                            href="mailto:matthewkingstudent@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="30"
+                                height="30"
+                                viewBox="0 0 16 16"
+                                fill="white"
+                            >
+                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
                 <form
                     className="form"
                     onSubmit={handleSubmit}

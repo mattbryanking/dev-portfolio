5711f878411df9a79421bdc57ee062739cf1f031
Matthew King <matthewkingstudent@gmail.com>
2025-01-31 23:30:44 -0500

    added mobile temp page

--- src/App.jsx
+++ src/App.jsx
@@ -6,129 +6,136 @@ import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import "./App.css";
+import Mobile from "./Components/Mobile/Mobile";
 
 const App = () => {
-    const [dark, setDark] = useState(true);
-    const [expanded, setExpanded] = useState(false);
+  const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
 
-    const upperRef = useRef(null);
-    const projectsRef = useRef(null);
-    const aboutRef = useRef(null);
-    const contactRef = useRef(null);
+  const [dark, setDark] = useState(true);
+  const [expanded, setExpanded] = useState(false);
 
-    const refs = {
-        upperRef,
-        projectsRef,
-        aboutRef,
-        contactRef,
+  const upperRef = useRef(null);
+  const projectsRef = useRef(null);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const refs = {
+    upperRef,
+    projectsRef,
+    aboutRef,
+    contactRef,
+  };
+
+  const scrollTo = (ref) => {
+    const yOffset = 5 * 16;
+    const y =
+      ref.current.getBoundingClientRect().top + window.pageYOffset - yOffset;
+
+    window.scrollTo({ top: y, behavior: "smooth" });
+  };
+
+  const toggleTheme = () => {
+    setExpanded(true);
+    console.log("toggleTheme");
+  };
+
+  useEffect(() => {
+    if (expanded) {
+      setTimeout(() => {
+        setExpanded(false);
+        setDark(!dark);
+      }, 1000);
+    }
+  }, [expanded]);
+
+  useEffect(() => {
+    const handleClipScroll = () => {
+      document.documentElement.style.setProperty(
+        "--scroll-y",
+        `${window.scrollY}px`
+      );
     };
 
-    const scrollTo = (ref) => {
-        const yOffset = 5 * 16;
-        const y =
-            ref.current.getBoundingClientRect().top +
-            window.pageYOffset -
-            yOffset;
+    window.addEventListener("scroll", handleClipScroll);
 
-        window.scrollTo({ top: y, behavior: "smooth" });
+    return () => {
+      window.removeEventListener("scroll", handleClipScroll);
     };
+  }, []);
 
-    const toggleTheme = () => {
-        setExpanded(true);
-        console.log("toggleTheme");
-    };
-
-    useEffect(() => {
-        if (expanded) {
-            setTimeout(() => {
-                setExpanded(false);
-                setDark(!dark);
-            }, 1000);
-        }
-    }, [expanded]);
-
-    useEffect(() => {
-        const handleClipScroll = () => {
-            document.documentElement.style.setProperty(
-                "--scroll-y",
-                `${window.scrollY}px`
-            );
-        };
-
-        window.addEventListener("scroll", handleClipScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleClipScroll);
-        };
-    }, []);
-
-    useEffect(() => {
-        const darkModeMediaQuery = window.matchMedia(
-            "(prefers-color-scheme: dark)"
-        );
-        setDark(darkModeMediaQuery.matches);
-
-        const handlePref = (event) => {
-            setDark(event.matches);
-        };
-
-        darkModeMediaQuery.addEventListener("change", handlePref);
-
-        return () => {
-            darkModeMediaQuery.removeEventListener("change", handlePref);
-        };
-    }, []);
-
-    return (
-        <>
-            <div
-                className={`App Light ${dark ? "clip" : ""} ${
-                    expanded ? "expanded" : ""
-                }`}
-            >
-                <div className="theme-toggle-wrapper">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 16 16"
-                        onClick={!dark ? toggleTheme : () => {}}
-                    >
-                        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-                    </svg>
-                </div>
-                <Navbar scroll={scrollTo} refs={refs} />
-                <Upper ref={upperRef} />
-                <Projects ref={projectsRef} />
-                <About ref={aboutRef} />
-                <Contact ref={contactRef} />
-                <Footer />
-            </div>
-            <div
-                className={`App Dark ${!dark ? "clip" : ""} ${
-                    expanded ? "expanded" : ""
-                }`}
-            >
-                <div className="theme-toggle-wrapper">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 16 16"
-                        onClick={dark ? toggleTheme : () => {}}
-                    >
-                        <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-                    </svg>
-                </div>
-                <Navbar scroll={scrollTo} refs={refs} />
-                <Upper ref={upperRef} darkMode={true} />
-                <Projects ref={projectsRef} />
-                <About ref={aboutRef} />
-                <Contact ref={contactRef} />
-                <Footer />
-            </div>
-        </>
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
     );
+    setDark(darkModeMediaQuery.matches);
+
+    const handlePref = (event) => {
+      setDark(event.matches);
+    };
+
+    darkModeMediaQuery.addEventListener("change", handlePref);
+
+    return () => {
+      darkModeMediaQuery.removeEventListener("change", handlePref);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <Mobile dark={dark} />
+    );
+  }
+
+  return (
+    <>
+      <div
+        className={`App Light ${dark ? "clip" : ""} ${
+          expanded ? "expanded" : ""
+        }`}
+      >
+        <div className="theme-toggle-wrapper">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 16 16"
+            onClick={!dark ? toggleTheme : () => {}}
+          >
+            <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+          </svg>
+        </div>
+        <Navbar scroll={scrollTo} refs={refs} />
+        <Upper ref={upperRef} />
+        <Projects ref={projectsRef} />
+        <About ref={aboutRef} />
+        <Contact ref={contactRef} />
+        <Footer />
+      </div>
+      <div
+        className={`App Dark ${!dark ? "clip" : ""} ${
+          expanded ? "expanded" : ""
+        }`}
+      >
+        <div className="theme-toggle-wrapper">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 16 16"
+            onClick={dark ? toggleTheme : () => {}}
+          >
+            <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+          </svg>
+        </div>
+        <Navbar scroll={scrollTo} refs={refs} />
+        <Upper ref={upperRef} darkMode={true} />
+        <Projects ref={projectsRef} />
+        <About ref={aboutRef} />
+        <Contact ref={contactRef} />
+        <Footer />
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/Components/Mobile/Mobile.jsx
@@ -0,0 +1,44 @@
+import "./Mobile.css";
+
+const Mobile = (dark) => {
+  return (
+    <div className={`Mobile ${dark ? "Dark" : "Light"}`}>
+
+        <h1>
+          This site cannot currently be viewed on mobile. Please view later on a
+          desktop computer.
+        </h1>
+        <div className="mobile-flex">
+        <a
+          href="/assets/MKing_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z" />
+          </svg>
+        </a>
+        <a
+          href="https://github.com/mattbryanking"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/matthewbryanking/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M22.23 0H1.77C.79 0 0 .77 0 1.72v20.56C0 23.23.79 24 1.77 24h20.46c.98 0 1.77-.77 1.77-1.72V1.72C24 .77 23.21 0 22.23 0zM7.06 20.45H3.56V9h3.5v11.45zM5.31 7.6c-1.12 0-2.03-.91-2.03-2.03 0-1.12.91-2.03 2.03-2.03s2.03.91 2.03 2.03c0 1.12-.91 2.03-2.03 2.03zM20.45 20.45h-3.5v-5.57c0-1.33-.03-3.05-1.85-3.05-1.85 0-2.13 1.44-2.13 2.94v5.68h-3.5V9h3.36v1.56h.05c.47-.89 1.6-1.83 3.29-1.83 3.53 0 4.18 2.32 4.18 5.34v6.38z" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Mobile;
